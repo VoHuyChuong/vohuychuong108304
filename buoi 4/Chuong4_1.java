8a8300a3f7bec12c84e16538bc9758b3368697b0
VoHuyChuong108304 <chuong108304@donga.edu.vn>
2024-10-19 23:35:13 +0700

    Add files via upload

---
+++ buoi 4/Chuong4_1.java
@@ -0,0 +1,16 @@
+package buoi4;
+public class Chuong4_1 {
+    public static void bai4_1() {
+        int arr[] = {3, 5, 8, 10, 14, 25};
+        int cs=0;
+        System.out.printf("Phần tử thứ nhất arr[%d] = " + arr[cs], cs++);
+        System.out.printf("\nPhần tử thứ hai arr[%d] = " + arr[cs], cs++);
+        System.out.printf("\nPhần tử thứ ba arr[%d] = " + arr[cs], cs++);
+        System.out.printf("\nPhần tử thứ tư arr[%d] = " + arr[cs], cs++);
+        System.out.printf("\nPhần tử thứ năm arr[%d] = " + arr[cs], cs++);
+        System.out.printf("\nPhần tử thứ sáu arr[%d] = " + arr[cs], cs++);
+    }
+    public static void main(String[] args) {
+        bai4_1();
+    }
+}
